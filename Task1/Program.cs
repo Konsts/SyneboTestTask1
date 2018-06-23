03644412facf22e70de6b9f09e0cd7c3f44c8b31
Konsts <konst.st.96@hotmail.com>
2018-06-23 18:49:58 +0300

    initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Squares(4, 17));
+            Console.ReadLine();
+        }
+        public static int Squares(int A, int B)
+        {
+            if (A > B)
+                throw new System.ArgumentException("arg A must be <= B");
+            return (int)Math.Sqrt(B) - (int)Math.Sqrt(A - 1);
+        }
+    }
+}
